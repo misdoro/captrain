65c0d6bc65803bd1592b9fc22c85bf0ab41843e1
Misha Doronin <misha@doronin.org>
2015-04-09 19:48:10 +0200

    Search recursively with up to 6 letters, in reverse order for now. Add space and - delimiters

--- main.rb
+++ main.rb
@@ -14,24 +14,43 @@ db=DBClient.new()
 
 #db.addStations(statns)
 
-
-('a'..'z').each do |l1|
-  ('a'..'z').each do |l2|
-    stn=l1+l2
-    print stn+"\n"
-    statns=apc.query_q(stn)
+def search_level(captclient, db, sstr)
+  depth=5
+  servlim=8
+  [*('a'..'z'),' ','-'].reverse.each do |ltr|
+    stn=sstr+ltr
+    puts stn
+    statns=captclient.query_q(stn)
     db.addStations(statns)
-    if statns.size>=8
-      ('a'..'z').each do |l3|
-	statns=apc.query_q(stn+l3)
-	 db.addStations(statns)
-	 puts "third loop"
-	 puts statns.inspect
-      end
-    end
-    #print statn
     sleep(1)
+    if statns.size>=servlim && stn.size<=depth
+      search_level(captclient,db,stn)
+    end
   end
 end
+
+#Recursive:
+search_level(apc,db,"")
+
+#7143 with three levels
+#('a'..'z').each do |l1|
+#  ('a'..'z').each do |l2|
+#    stn=l1+l2
+#    print stn+"\n"
+#    statns=apc.query_q(stn)
+#    db.addStations(statns)
+#    if statns.size>=8
+#      ('a'..'z').each do |l3|
+#	statns=apc.query_q(stn+l3)
+#	 db.addStations(statns)
+#	 puts "third loop "+stn+l3
+#	 puts statns.inspect
+#	sleep(1)
+#      end
+#    end
+#    #print statn
+#    sleep(1)
+#  end
+#end
 
 db.close()
